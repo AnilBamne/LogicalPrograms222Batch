0bd01c6a2a1f52b313b7f4108f6b7ea25cb59210
AnilBamne <anilbamne20@gmail.com>
2022-11-15 17:12:18 +0530

    Adding Welcome message

---
+++ LogicalPrograms222Batch/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LogicalPrograms222Batch
+{
+    internal class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logiocal Programs");
+            
+        }
+    }
+}
