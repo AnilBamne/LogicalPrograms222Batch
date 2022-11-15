90828859baf1a7a114d21dc941c82f8de8a21f27
AnilBamne <anilbamne20@gmail.com>
2022-11-15 17:35:19 +0530

    Adding FibonacciSeries code

---
+++ LogicalPrograms222Batch/FibonacciSeries.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms222Batch
+{
+    internal class FibonacciSeries
+    {
+        public void fibonacciSeries()
+        {
+            int firstNumber = 1, SecondNumber = 2, nextNumber, numberOfElements;
+            Console.Write("Enter the number of elements to Print : ");
+            numberOfElements = int.Parse(Console.ReadLine());
+            if (numberOfElements < 2)
+            {
+                Console.Write("Please Enter a number greater than two");
+            }
+            else
+            {
+                //First print first and second number
+                Console.WriteLine(firstNumber );
+                Console.WriteLine( SecondNumber);
+                //Starts the loop from 2 because 0 and 1 are already printed
+                for (int i = 2; i <= numberOfElements; i++)
+                {
+                    nextNumber = firstNumber + SecondNumber;
+                    Console.WriteLine(nextNumber + " ");
+                    firstNumber = SecondNumber;
+                    SecondNumber = nextNumber;
+                }
+            }
+        }
+    }
+}
--- LogicalPrograms222Batch/Program.cs
+++ LogicalPrograms222Batch/Program.cs
@@ -10,7 +10,24 @@ namespace LogicalPrograms222Batch
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Logiocal Programs");
-            
+            Console.WriteLine("Select the Program to execute\n\n1:Fibonacci Series\n2:Perfect Number\n3:Prime Number\n4:Reverse a number\n5:Coupon Numbers\n6:Simulate Stopwatch Program\n ");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    FibonacciSeries series = new FibonacciSeries();
+                    series.fibonacciSeries();
+                    break;
+                case 2:
+                   
+                    break;
+                
+                
+                default:
+                    break;
+            }
+            Console.WriteLine("**************************");
+
         }
     }
 }
